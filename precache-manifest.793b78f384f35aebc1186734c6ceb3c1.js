78d1803a999b8271a3f6ac7ab1f9c3523a84b134
Frank Lyder Bredland <fl@nyggen.com>
2019-08-13 17:05:53 +0200

    Updates

---
+++ precache-manifest.793b78f384f35aebc1186734c6ceb3c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f526f0ca7dd41e0a0f59faf8cc313854",
+    "url": "/tobben-summerprojects-2019/index.html"
+  },
+  {
+    "revision": "76f3ed62c3e149d1bae8",
+    "url": "/tobben-summerprojects-2019/static/css/2.5cebe70d.chunk.css"
+  },
+  {
+    "revision": "76f3ed62c3e149d1bae8",
+    "url": "/tobben-summerprojects-2019/static/js/2.5e239a7c.chunk.js"
+  },
+  {
+    "revision": "80a274eba106575e7d7e",
+    "url": "/tobben-summerprojects-2019/static/js/main.6103193a.chunk.js"
+  },
+  {
+    "revision": "f53941401b7b2d8a0971",
+    "url": "/tobben-summerprojects-2019/static/js/runtime~main.a28c42b4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b0205852ed62b343d0d39833e99a6e3.js"
+  "/tobben-summerprojects-2019/precache-manifest.793b78f384f35aebc1186734c6ceb3c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tobben-summerprojects-2019/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
