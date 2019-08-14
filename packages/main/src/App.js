35861a62dbcb9d6ebaa9c73a9a11d89f1034bf24
Frank Lyder Bredland <fl@nyggen.com>
2019-08-14 19:03:37 +0200

    New presentation version

--- packages/main/src/App.js
+++ packages/main/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Presentation, Slideset, Slide } from 'react-presentation';
+import { Presentation, Slide } from 'react-presentation';
 import 'react-presentation/lib/index.css';
 //import Highlight from 'react-highlight';
 
@@ -8,24 +8,21 @@ const Link = ({ href }) =>
 
 export default () =>
 	<Presentation>
-		<Slideset i={0}>
-			<Slide>
-				<h1>Summer Projects 2019</h1>
-				<h2>By Frank Lyder Bredland</h2>
-			</Slide>
-			<Slide>
-				<h1>Projects</h1>
-				<ul>
-					<li>Isomorphic JSX</li>
-					<li>RavenDesk</li>
-					<li>Queryable</li>
-					<li>Codemodr</li>
-					<li>And others...</li>
-				</ul>
-			</Slide>
-		</Slideset>
-		<Slideset i={1}>
-			<Slide>
+		<Slide>
+			<h1>Summer Projects 2019</h1>
+			<h2>By Frank Lyder Bredland</h2>
+		</Slide>
+		<Slide>
+			<h1>Projects</h1>
+			<ul>
+				<li>Isomorphic JSX</li>
+				<li>RavenDesk</li>
+				<li>Queryable</li>
+				<li>Codemodr</li>
+				<li>And others...</li>
+			</ul>
+		</Slide>
+		<Slide>
 				<p>
 					<img src="https://raw.githubusercontent.com/TheKnarf/isomorphic-jsx/master/logo/logo.png" alt="isomorphic-jsx" />
 				</p>
@@ -33,50 +30,43 @@ export default () =>
 				<p>A JSX to string library</p>
 				<div>
 					<p>Use cases:</p>
-					<ol style={{ display: 'inline-block', 'text-align': 'left' }}>
+					<ol style={{ display: 'inline-block', textAlign: 'left' }}>
 						<li>Usage with Express.js</li>
 						<li>Static site generator</li>
 						<li>XML</li>
 					</ol>
 				</div>
-			</Slide>
-		</Slideset>
-		<Slideset i={2}>
-			<Slide>
+		</Slide>
+		<Slide>
 				<p>
 					<img src="https://raw.githubusercontent.com/TheKnarf/RavenDesk/master/logo/Logotype102.png" alt="RavenDesk" />
 				</p>
 				<p><Link href="https://github.com/TheKnarf/ravendesk" /></p>
-			</Slide>
-		</Slideset>
-		<Slideset i={3}>
-			<Slide>
+		</Slide>
+		<Slide>
 				<h1>Queryable</h1>
 				<p><Link href="https://github.com/theknarf-experiments/queryable" /></p>
-			</Slide>
-		</Slideset>
-		<Slideset i={4}>
-			<Slide>
-				<h1>And others...</h1>
-				<p>Quick round of a few other projects</p>
-			</Slide>
-			<Slide>
-				<p>
-					<img 
-						src="https://raw.githubusercontent.com/TheKnarf/HeliumAnion/master/logo/HeliumAnion.png"
-						alt="HeliumAnion"
-						style={{ width: '50vh' }}
-						/>
-				</p>
-				<p><Link href="https://github.com/TheKnarf/heliumanion" /></p>
-			</Slide>
-			<Slide>
-				<h1> React Presentation </h1>
-				<p><Link href="https://github.com/theknarf/react-presentation" /></p>
-			</Slide>
-			<Slide>
-				<h1> React Terminal </h1>
-				<p><Link href="https://github.com/theknarf/react-terminal" /></p>
-			</Slide>
-		</Slideset>
+		</Slide>
+		<Slide>
+			<h1>And others...</h1>
+			<p>Quick round of a few other projects</p>
+		</Slide>
+		<Slide>
+			<p>
+				<img
+					src="https://raw.githubusercontent.com/TheKnarf/HeliumAnion/master/logo/HeliumAnion.png"
+					alt="HeliumAnion"
+					style={{ width: '50vh' }}
+					/>
+			</p>
+			<p><Link href="https://github.com/TheKnarf/heliumanion" /></p>
+		</Slide>
+		<Slide>
+			<h1> React Presentation </h1>
+			<p><Link href="https://github.com/theknarf/react-presentation" /></p>
+		</Slide>
+		<Slide>
+			<h1> React Terminal </h1>
+			<p><Link href="https://github.com/theknarf/react-terminal" /></p>
+		</Slide>
 	</Presentation>;
